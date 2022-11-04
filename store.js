c5bdddd1df10dd74961e8e3643686ec959a6d7f2
Oluwatimilehin AjeOjo <timiajeojo@gmail.com>
2022-11-04 23:07:41 +0100

    last one

--- store.js
+++ store.js
@@ -76,7 +76,7 @@
         var cartRowContents = `
             <div class="cart-item cart-column">
                 <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-                <span class="cart-item-title">${title}</span>
+                <span class="cart-item-title"> $ {title}</span>
             </div>
             <span class="cart-price cart-column">${price}</span>
             <div class="cart-quantity cart-column">
